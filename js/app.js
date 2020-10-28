cdff10793893b3887552c742b7e423495c72c4b0
IvanVeskovic <weeskaa@gmail.com>
2020-10-28 13:05:58 +0100

    last v

--- js/app.js
+++ js/app.js
@@ -1,3 +1,164 @@
-const cont = document.querySelector('.container');
+const movieList = document.querySelector('.list');
+const movies = document.querySelectorAll('.movie');
+const pageMovie = document.querySelector('.page__movie');
+const pageAbout = document.querySelector('.page__about');
+const pageBook = document.querySelector('.page__book');
+const seatsBox = document.querySelector('.seats__box');
 
-cont.querySelector('img').src = data[0].img;
+const displayMoviesFromDb = () => {
+    for(let i = 0; i < data.length; i++) {
+        createMovieForList(data[i]);
+    }
+}
+
+const createMovieForList = (movie) => {
+    const movieDiv = document.createElement('div');
+    const {id, name, img, genre, rating, price, director, stars, action} = movie;
+    
+    movieDiv.classList.add('movie');
+    movieDiv.setAttribute("id", `${id}`);
+
+    movieDiv.innerHTML = `
+                    <div class="movie__price">
+                        ${price}
+                    </div>
+                    
+                    <img src="${img}" alt="movie" class="movie__img">
+                    
+                    <div class="content">
+                        <h3 class="heading heading__third">${name}</h3>
+                        <div class="movie__about">
+                            <span class="strong">
+                                Genre:
+                            </span>
+                            ${loopArr(genre)}
+                        </div>
+
+                        <div class="movie__about">
+                            <span class="strong">
+                                Rating:
+                            </span>
+                            ${rating}
+                        </div>
+
+                        <div class="movie__about">
+                            <span class="strong">
+                                Director:
+                            </span>
+                            ${director}
+                        </div>
+
+                        <div class="movie__about">
+                            <span class="strong">
+                                Stars:
+                            </span>
+                            ${loopArr(stars)}
+                        </div>
+                    </div>
+
+                    <div class="actions actions__movie">
+                        <a href="#" class="btn btn__white more">more</a>
+                        <a href="#" class="btn btn__pink book">book now</a>
+                    </div>
+    `
+
+    movieList.appendChild(movieDiv);
+}
+
+
+
+const loopArr = (arr) => {
+    let stringedArr = '';
+    arr.forEach(el => {
+        stringedArr += ` ${el},`
+    });
+
+    return stringedArr.slice(0, -1);
+}
+
+const showAboutPageWithMovie = (id) =>  {
+    const movie = data.find(el => el.id === id);
+    const {name, img, genre, rating, price, director, stars, action} = movie;
+
+    document.getElementById('about_img').src = img;
+    document.getElementById('about_title').innerText = name;
+    document.getElementById('about_action').innerText = action;
+    document.getElementById('about_genre').innerText = loopArr(genre);
+    document.getElementById('about_rating').innerText = rating;
+    document.getElementById('about_director').innerText = director;
+    document.getElementById('about_stars').innerText = loopArr(stars);
+}
+
+const showBookPageWithSeats = (id) => {
+    const movie = data.find(el => el.id === id);
+    
+    const bookTitle = document.getElementById('book_title');
+    const bookImg = document.getElementById('book_img');
+    const bookPrice = document.getElementById('book_price');
+
+    const {name, img, price} = movie;
+
+    bookTitle.innerText = name;
+    bookImg.src = img;
+    bookPrice.innerText = price;
+}
+
+// Update total price based on movie seats and price
+const updateTotalPrice = () => {
+    const seats = seatsBox.querySelectorAll('SVG .seats__seat--selected').length;
+    const price = +pageBook.querySelector('#book_price').innerText.substring(1);
+    const total = pageBook.querySelector('#book_total');
+    const tickets = pageBook.querySelector('#book_count');
+    const totalNum = seats * price;
+    
+    tickets.innerText = seats;
+    total.innerText = '$' + totalNum;
+}
+
+// ####################### LISTENERS #########################
+
+// Open page about with movie dets
+movieList.addEventListener('click', function(e) {
+    
+    const movieId = e.target.parentElement.parentElement.id;
+    if(e.target.classList.contains('more')){
+        pageAbout.classList.add('active');
+        pageMovie.classList.remove('active');
+        pageBook.classList.remove('active');
+
+        showAboutPageWithMovie(movieId)
+    } else if(e.target.classList.contains('book')){
+        pageBook.classList.add('active');
+        pageMovie.classList.remove('active');
+        pageAbout.classList.remove('active');
+        
+        
+        showBookPageWithSeats(movieId);
+
+    }
+    e.preventDefault();
+})
+
+// Back from page about to movie list 
+document.querySelector('.back').addEventListener('click', function() {
+    pageMovie.classList.add('active');
+    pageAbout.classList.remove('active');
+});
+
+// Toogle seats on click
+seatsBox.querySelectorAll('svg').forEach(el => {
+    el.addEventListener('click', function(e){
+        if(!(e.target.classList.contains('seats__seat--booked') || e.target.parentElement.classList.contains('seats__seat--booked'))){
+            e.target.classList.toggle('seats__seat--selected');
+            updateTotalPrice();
+        }
+    })
+});
+
+// Back to main
+document.querySelector('.header__back').addEventListener('click', function(){
+    pageBook.classList.remove('active');
+    pageMovie.classList.add('active');
+})
+
+displayMoviesFromDb();
--- js/data.js
+++ js/data.js
@@ -7,7 +7,51 @@ const data = [
         rating: 5.4,
         price: '$10',
         director: 'Niki Caro',
-        stars: 'Yifei Liu, Donnie Yen, Li Gong',
-        action: 'A young Chinese maiden disguises herself as a male warrior in order to save her father.'
-    }
+        stars: ['Yifei Liu', 'Donnie Yen', 'Li Gong'],
+        action: 'In rural imperial China, Hua Mulan is an adventurous and active girl, to the disappointment of her mother who hopes that one day she will be wed to a good husband. Once Mulan reaches young adulthood, she is forced to meet with a matchmaker and bring honor to her family. As China is at brink of war, Imperial soldiers arrive in Mulan’s village to enlist recruits, and her elderly and frail father, Zhou, is forced into the ranks as he is son-less. Mulan realizes that her father has no chance of survival, flees with his armor, horse, and sword to join in his place under disguise. Mulan arrives at the training camp, which is run by Commander Tung, who had previously served with Zhou in the previous war. Mulan hides her gender and succeeds in training. Mulan follows the journey of it’s lead Hua Mulan into accepting who she really is instead of hiding away.'
+    },
+    {
+        id: '2',
+        name: 'Tenet',
+        img: 'img/tenet.jpg',
+        genre: ['Action', 'Sci-Fi'],
+        rating: 7.8,
+        price: '$14',
+        director: 'Christopher Nolan',
+        stars: ['John David Washington', 'Robert Pattinson', 'Elizabeth Debicki'],
+        action: 'An unnamed CIA agent—”The Protagonist”—participates in an undercover operation at a Kiev opera house. He is aided by a masked soldier with a distinctive trinket, who appears to “un-fire” a bullet through a hostile gunman.After rescuing an exposed spy and seizing a strange artifact, the Protagonist is captured by Russian mercenaries. He endures torture before consuming a cyanide pill. He later awakens to learn the pill was fake; his team has been killed and the artifact lost. The Protagonist learns that he is now employed by a secret organization called “Tenet,” whose mission involves the human race’s survival. He is directed to Barbara, a scientist studying bullets with “inverted” entropy which allows them to move backwards through time. She believes they are manufactured in the future, and there exists a weapon that can wipe out the past.'
+    },
+    {
+        id: '3',
+        name: 'The War With Grandpa',
+        img: 'img/the_war_with_grandpa.jpg',
+        genre: ['Comedy', 'Drama', 'Family'],
+        rating: 5.5,
+        price: '$11',
+        director: 'Tim Hill',
+        stars: ['Robert De Niro', 'Uma Thurman', 'Rob Riggle '],
+        action: 'Don’t miss the laugh-out-loud classic about a boy who leaps into battle when he’s forced to share a room with his grandfather–now a major motion picture starring Robert De Niro, Uma Thurman, Christopher Walken, Jane Seymour, Rob Riggle, Cheech Marin, and Oakes Fegley! Peter is thrilled that Grandpa Jack is coming to live with his family. That is, until Grandpa Jack moves right into Peter’s room. Like any grandson, he loves his grandpa, but…he wants his room back. Now Peter has no choice–he declares war! Peter begins to devise outrageous plots with the help of his friends to make Grandpa surrender the room. But even though the pranks escalate, Grandpa Jack is tougher than he looks. Rather than give in, Grandpa Jack plans to get even. They used to be such great pals, but Peter is now wondering–has their war gone too far?'
+    },
+    {
+        id: '4',
+        name: 'The Secret Garden',
+        img: 'img/the_secret_garden.jpg',
+        genre: ['Drama', 'Family', 'Fantasy'],
+        rating: 5.5,
+        price: '$8',
+        director: 'Marc Munden',
+        stars: ['Dixie Egerickx', 'Richard Hansell', 'David Verrey'],
+        action: 'In 1947, Mary Lennox is found abandoned, in her home in British India, her parents having died from cholera and she is forgotten in the turmoil of partition. Mary is sent away to her uncle, Lord Archibald Craven\'s Misselthwaite Manor in Yorkshire, England. She is an unpleasant and unkind young girl who has had to repress her own emotions whilst growing up in the Raj. Upon arriving in England she meets Mrs Medlock, a strict and firm lady who is Lord Craven\'s servant and housekeeper. Mary is instructed to not explore the house and is confined to her room at night. There, she meets Martha, a servant who is unsettled by her demands. Mary is allowed to leave the house to explore the estate and woods nearby and stumbles upon a stray dog whom she names Jemima. Later that night, she hears tiny screams and wailings throughout the corridors only to find Lord Craven\'s bedridden son, Colin Craven. The next day, Lord Craven sees Mary in his study and confides in her to not cause any trouble. Mary continues her escapade of heading to the forest in which she finds a hidden garden by climbing a wall. Later, on the way home, she calls out to Martha\'s brother Dickon who fades within the mist of the moors. Later she finds Jemima\'s leg caught in a trap. She helps Jemima by freeing her from the trap but the dog runs away into the garden. Subsequently Mary continues to explore the garden'
+    },
+    {
+        id: '5',
+        name: 'The One And Only Ivan',
+        img: 'img/the_one_and_only_ivan.jpg',
+        genre: ['Drama', 'Family', 'Fantasy'],
+        rating: 6.7,
+        price: '$13',
+        director: 'Thea Sharrock',
+        stars: ['Sam Rockwell', 'Bryan Cranston', 'Phillipa Soo'],
+        action: 'Ivan, a silverback western lowland gorilla, lives in the enclosure at Big Top Mall. He performs for Mack, the owner, alongside other performers; a wise and nurturing African elephant named Stella, a spoiled poodle named Snickers, a spunky chicken named Henrietta, a fire truck-riding rabbit named Murphy, a paranoid sea lion named Frankie and a loquacious parrot named Thelma. Ivan is accompanied by a stray mutt, who they later name Bob, who secretly lives in his enclosure. Mack\'s janitorial employee George, brings his daughter Julia who frequently interacts with Ivan. One day, Julia leaves her old crayons with Ivan who suddenly begins taking an interest in drawing with them. Julia is apparently the only one who can decipher his drawings and what they mean. The Big Top Mall begins losing business, much to Mack\'s concern, but becomes excited when he purchases a baby elephant named Ruby who begins to draw in a crowd. Ivan is initially hesitant over accepting Ruby, especially when she begins to pry into his personal life. When Stella, who has been caring for Ruby, suddenly becomes weaker and unable to perform, Ivan gives Ruby encouragement to embrace the crowd on her own. Afterwards, Ruby asks Ivan to tell her a story and Ivan reveals how Mack and his wife Helen adopted him when he was just a baby. As Ivan got bigger, Mack became forgiving of his animalistic behavior, but Helen left him out of frustration. Mack eventually purchased the mall and turned it into a circus. Stella asks Ivan to look after Ruby and make sure she is free before passing away.'
+    },
 ]
